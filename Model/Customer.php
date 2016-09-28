7853dc9b930e4afcaad1651a4ae69e912913aaa4
Jeff Britts <jbritts@magento.com>
2016-09-28 08:04:00 -0400

    added preference for company name in welcome message

---
+++ Model/Customer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MagentoEse\MyAccountWidget\Model;
+
+use Magento\Customer\Helper\Session\CurrentCustomer;
+use Magento\Customer\Helper\View;
+
+/**
+ * Customer section
+ */
+class Customer implements \Magento\Customer\CustomerData\SectionSourceInterface
+{
+    /**
+     * @var CurrentCustomer
+     */
+    protected $currentCustomer;
+    protected $companyAttributes;
+    protected $companyRepository;
+
+    /**
+     * @param CurrentCustomer $currentCustomer
+     * @param View $customerViewHelper
+     */
+    public function __construct(
+        CurrentCustomer $currentCustomer,
+        View $customerViewHelper,
+        \Magento\Company\Model\Customer\CompanyAttributes $companyAttributes,
+        \Magento\Company\Model\CompanyRepository $companyRepository
+
+    ) {
+        $this->currentCustomer = $currentCustomer;
+        $this->customerViewHelper = $customerViewHelper;
+        $this->companyAttributes = $companyAttributes;
+        $this->companyRepository = $companyRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSectionData()
+    {
+        if (!$this->currentCustomer->getCustomerId()) {
+            return [];
+        }
+        $customer = $this->currentCustomer->getCustomer();
+        return [
+            'fullname' => $this->customerViewHelper->getCustomerName($customer),
+            'firstname' => $customer->getFirstname(),
+            'company' => $this->companyRepository->get($this->companyAttributes->getCompanyAttributesByCustomer($customer)->getCompanyId())->getName(),
+        ];
+    }
+}
