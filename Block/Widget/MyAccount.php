4e2cc73ca690a674621a57a9ccefc57ad0f8d3ac
Jeffrey Britts <jbritts@magento.com>
2017-09-19 09:20:03 -0400

    non knockout version

--- Block/Widget/MyAccount.php
+++ Block/Widget/MyAccount.php
@@ -1,10 +1,66 @@
 <?php
 namespace MagentoEse\MyAccountWidget\Block\Widget;
+
 class MyAccount extends \Magento\Framework\View\Element\Template implements \Magento\Widget\Block\BlockInterface
 {
-    protected function _construct()
+    /**
+     * @var \Magento\Framework\View\Element\Template\Context
+     */
+    protected $context;
+
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
+    protected $session;
+
+    /**
+     * @var \MagentoEse\MyAccountWidget\Model\CustomerOrder
+     */
+    protected $customerOrder;
+
+    /**
+     * @var
+     */
+    protected $data;
+
+
+    /**
+     * MyAccount constructor.
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $session
+     * @param \MagentoEse\MyAccountWidget\Model\CustomerOrder $customerOrder
+     * @param array $data
+     */
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context,
+                                \Magento\Customer\Model\Session $session,
+                                \MagentoEse\MyAccountWidget\Model\CustomerOrder $customerOrder,
+                                array $data = [])
     {
-        parent::_construct();
+
         $this->setTemplate('widget/my_account.phtml');
+        $this->session = $session;
+        $this->customerOrder = $customerOrder;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isloggedIn(){
+       return $this->session->isLoggedIn();
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrders(){
+        return $this->customerOrder->getOrders();
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreditLimit(){
+        return $this->customerOrder->getCreditLimit();
     }
 }
